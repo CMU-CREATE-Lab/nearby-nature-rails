c90be911f35b07f74273214e35b28a3f11ef0468
mtasota <miketasota@gmail.com>
2017-11-03 23:46:33 +0000

    Added javascript action when button is clicked to generate embed code output.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+var clickGenerateCode = function() {
+  var url = $("#input-url").val();
+  var link_text = $("#input-link-text").val() == "" ? "Click to input data" : $("#input-link-text").val();
+  var embed_code = $("#input-embedcode").val();
+  var result = "<a href=\""+url+"\">"+link_text+"</a><br/>"+embed_code;
+  $("#textarea-output").text(result);
+}
+
+
+$(document).ready(function() {
+  $("#input-generate").on("click", clickGenerateCode);
+});
